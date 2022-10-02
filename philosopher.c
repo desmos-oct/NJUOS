5df5dd7680acdce7dc84ce0a197a476dcf8302d2
Vermouth <desmosoct@gmail.com>
2022-10-02 22:18:11 +0800

    delete

--- philosopher.c
+++ philosopher.c
@@ -1,19 +1,34 @@
 #include "thread.h"
 #include "thread-sync.h"
+#include<unistd.h>
 
-#define N 3
+#define N 5
 sem_t locks[N];
+mutex_t mutex = MUTEX_INIT();
+cond_t cv = COND_INIT();
+
+spinlock_t lock = SPIN_INIT();
 
 void Tphilosopher(int id) {
   int lhs = (N + id - 1) % N;
   int rhs = id % N;
   while (1) {
-    P(&locks[lhs]);
-    printf("T%d Got %d\n", id, lhs + 1);
-    P(&locks[rhs]);
-    printf("T%d Got %d\n", id, rhs + 1);
-    V(&locks[lhs]);
-    V(&locks[rhs]);
+    mutex_lock(&mutex);
+    while (!(locks[lhs].__align && locks[rhs].__align)) {
+      printf("%d fail",id);	    
+      cond_wait(&cv, &mutex);
+    }
+    locks[lhs].__align = locks[rhs].__align = 0;
+    printf("%d got %d and %d\n", id, lhs + 1, rhs + 1);
+    mutex_unlock(&mutex);
+
+    usleep(10);
+
+    mutex_lock(&mutex);
+    locks[lhs].__align = locks[rhs].__align = 1;
+    printf("%d release %d and %d\n", id, lhs + 1, rhs + 1);
+    cond_broadcast(&cv);
+    mutex_unlock(&mutex);
   }
 }
 
